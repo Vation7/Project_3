6522aac52d8b58b807b006d6135776afe989405b
Miguel <gawerne60@icloud.com>
2024-09-22 22:27:15 -0600

    new changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,6 +25,19 @@ export const ADD_FRIEND = gql`
   }
 `;
 
+export const REMOVE_FRIEND = gql`
+  mutation removeFriend($friendId: ID!) {
+    removeFriend(friendId: $friendId) {
+      _id
+      username
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -77,19 +90,6 @@ export const LIKE_THOUGHT = gql`
       thoughtAuthor
       createdAt
       likes {
-        _id
-        username
-      }
-    }
-  }
-`;
-
-export const REMOVE_FRIEND = gql`
-  mutation removeFriend($friendId: ID!) {
-    removeFriend(friendId: $friendId) {
-      _id
-      username
-      friends {
         _id
         username
       }
